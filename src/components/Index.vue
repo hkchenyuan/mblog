c400fdb7fbcf663898676c1becddbf450fede505
liyuequn <18511909124@163.com>
2017-08-16 15:10:31 +0800

    第二次提交

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,5 +1,28 @@
 <template>
-
+    <div>
+        <mt-tabbar v-model="selected">
+        <mt-tab-item id="tab1">
+            <img slot="icon" src="">
+            李
+        </mt-tab-item>
+        <mt-tab-item id="tab2">
+            <img slot="icon" src="">
+            岳
+        </mt-tab-item>
+        <mt-tab-item id="tab3">
+            <img slot="icon" src="">
+            群
+        </mt-tab-item>
+        <mt-tab-item id="tab4">
+            <img slot="icon" src="">
+            最
+        </mt-tab-item>
+        <mt-tab-item id="tab5">
+            <img slot="icon" src="">
+            帅
+        </mt-tab-item>
+    </mt-tabbar>
+    </div>
 </template>
 
 <script>
@@ -7,7 +30,8 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+        selected:[],
     }
   }
 }

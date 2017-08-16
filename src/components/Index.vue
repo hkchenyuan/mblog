8665aee07c542306593678b7f13bdca661669932
liyuequn <18511909124@163.com>
2017-08-16 16:13:38 +0800

    导航栏小图标

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,25 +1,25 @@
 <template>
     <div>
+        <router-view></router-view>
         <mt-tabbar v-model="selected">
         <mt-tab-item id="tab1">
-            <img slot="icon" src="">
-            李
+            <img slot="icon" src="../assets/icon/index.png">
+            <span style="color: #26A2FF;">首页</span>
         </mt-tab-item>
         <mt-tab-item id="tab2">
-            <img slot="icon" src="">
-            岳
+            <img slot="icon" src="../assets/icon/message.png">
+            <span style="color: #26A2FF;">消息</span>
         </mt-tab-item>
         <mt-tab-item id="tab3">
-            <img slot="icon" src="">
-            群
+            <img slot="icon" src="../assets/icon/add.png">
         </mt-tab-item>
         <mt-tab-item id="tab4">
-            <img slot="icon" src="">
-            最
+            <img slot="icon" src="../assets/icon/find.png">
+            <span style="color: #26A2FF;">发现</span>
         </mt-tab-item>
         <mt-tab-item id="tab5">
-            <img slot="icon" src="">
-            帅
+            <img slot="icon" src="../assets/icon/user.png">
+            <span style="color: #26A2FF;">我</span>
         </mt-tab-item>
     </mt-tabbar>
     </div>
